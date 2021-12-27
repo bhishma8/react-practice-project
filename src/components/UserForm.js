a1e01ef7cea52a39c0af9194577262db5b9d4f12
bhishma8 <ishan.baban@gmail.com>
2021-12-27 20:26:17 +0530

    form input state

--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -1,18 +1,42 @@
-import React from 'react';
+import React,{useState} from 'react';
+
+import Card from './Card';
+import classes from './UserForm.module.css'
 function UserForm(){
 
+    const [userName,setUserName]=useState('');
+    const [age,setAge]=useState('');
     const formSubmitHandler=(event)=>
     {
         event.preventDefault();
+        if(userName.trim().length===0 || age.trim().length===0)
+        {
+            return;
+        }
+        if(+age<1)
+        {
+            return;
+        }
+        console.log(userName,age);
+        setUserName('');
+        setAge('');
+    }
+    const userNameChangeHandler=(event)=>{
+        setUserName(event.target.value);
+    }
+    const ageChangeHandler=event=>{
+        setAge(event.target.value);
     }
         return(
+            <Card className={classes.input}>
             <form onSubmit={formSubmitHandler}>
                 <label htmlFor='username'>Username</label>
-                <input type='text' id='username'></input>
+                <input type='text' id='username' onChange={userNameChangeHandler} value={userName}></input>
                 <label htmlFor='age'>Age</label>
-                <input type='number' id='age'></input>
+                <input type='number' id='age' onChange={ageChangeHandler} value={age}></input>
                 <button type='submit' >Add User</button>
             </form>
+            </Card>
         );
 }
-export default UserForm
+export default UserForm;
